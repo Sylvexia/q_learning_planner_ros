b17aa07f7bf3d6e15aee5ec339e8a5d987830a08
Chartreuse Sylvex <a1085125@mail.nuk.edu.tw>
2022-04-04 02:28:29 +0800

    fixed the order of spinOnce, added the key control planner halt, save, and quit

--- src/planner/planner.cpp
+++ src/planner/planner.cpp
@@ -4,17 +4,56 @@ int main(int argc, char **argv)
 {
     ros::init(argc, argv, "rl_planner");
     ros::NodeHandle nh;
-    ros::Rate loop_rate(10);
+    ros::Rate loop_rate(1);
 
-    Planner planner;
+    bool save = false;
+    bool exit = false;
+    bool suspend = false;
 
-    planner.create();
+    Planner planner(nh);
 
-    while (ros::ok())
+    while (!exit)
     {
-        planner.execute();
-        ros::spinOnce();
-        loop_rate.sleep();
+        save = false;
+        planner.init();
+
+        while (!save && !exit)
+        {
+            ros::spinOnce();
+            planner.execute();
+            loop_rate.sleep();
+
+            if (kbhit())
+            {
+                switch (tolower(getch()))
+                {
+                case 's':
+                {
+                    planner.save();
+                    save = true;
+                    suspend = true;
+                    while (suspend)
+                    {
+                        ROS_INFO("Suspending, press S to resume");
+                        loop_rate.sleep();
+                        if (kbhit() && getch() == 's')
+                        {
+                            suspend = false;
+                            ROS_INFO("Resume!");
+                        }
+                    }
+                    break;
+                }
+                case 27:
+                {
+                    planner.save();
+                    exit = true;
+                    break;
+                }
+                }
+            }
+        }
     }
+
     return 0;
 }
