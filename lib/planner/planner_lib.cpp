3de8c0c7cbe1c1a4701eeff2bbfc4ff805107857
Chartreuse Sylvex <a1085125@mail.nuk.edu.tw>
2022-04-04 02:30:53 +0800

    Fixed the misunderstood subscribe, execute function now would not re-sub and re-advert all the time

--- lib/planner/planner_lib.cpp
+++ lib/planner/planner_lib.cpp
@@ -6,7 +6,7 @@ Planner::Planner()
 }
 
 Planner::Planner(ros::NodeHandle &nh)
-    : m_nh(nh)
+    : m_nh(nh), m_state{{{0, 0}}}, m_action{{{0, 0, 0}}}
 {
     ROS_INFO("Class planner has been constructed");
 }
@@ -16,25 +16,32 @@ Planner::~Planner()
     ROS_INFO("Class planner has been destroyed");
 }
 
-void Planner::create()
+void Planner::init()
 {
     m_rand_gen = std::mt19937(static_cast<std::size_t>(std::chrono::high_resolution_clock::now()
                                                            .time_since_epoch()
                                                            .count()));
+
+    m_sub_state = m_nh.subscribe("/state", 1, &Planner::state_callback, this);
+    m_sub_reward = m_nh.subscribe("/reward", 1, &Planner::reward_callback, this);
+
+    m_pub_action = m_nh.advertise<reinforcement_learning_planner::action>("/action", 1);
 }
 
 void Planner::execute()
 {
     //sense();
-    m_sub_state = m_nh.subscribe("/state", 1, &Planner::state_callback, this);
-    m_sub_reward = m_nh.subscribe("/reward", 1, &Planner::reward_callback, this);
-
+    get_state();
+    get_reward();
     //plan();
     plan_q_learning();
-
     //act();
-    m_pub_action = m_nh.advertise<reinforcement_learning_planner::action>("/action", 1);
     set_action();
+}
+
+void Planner::save()
+{
+    ROS_INFO("Saving model");
 }
 
 void Planner::plan_q_learning()
@@ -63,7 +70,7 @@ void Planner::get_reward()
 }
 
 void Planner::set_action()
-{
-    ROS_INFO("Setting action:");
+{   
     m_pub_action.publish(m_action_msg);
+    ROS_INFO("Setting action:%d", m_action_msg.linear_action);
 }
